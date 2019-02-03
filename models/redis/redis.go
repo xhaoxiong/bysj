8d5941e1f67b19002a8a71f1f03924fca34adacf
haoxiongxiao <269812428@qq.com>
2019-02-03 19:32:55 +0800

    添加redis的增删查改

---
+++ models/redis/redis.go
@@ -0,0 +1,110 @@
+/**
+*@Author: haoxiongxiao
+*@Date: 2019/2/3
+*@Description: CREATE GO FILE redis
+*/
+package redis
+
+import (
+	"bysj/models"
+	"github.com/lexkong/log"
+	"github.com/garyburd/redigo/redis"
+	"encoding/json"
+	"errors"
+)
+
+func Set(k, v string) {
+	c := models.DB.Redis.Get()
+	defer c.Close()
+	_, err := c.Do("SET", k, v)
+	if err != nil {
+		log.Error("set error", err)
+	}
+}
+
+func GetStringValue(k string) string {
+	c := models.DB.Redis.Get()
+	defer c.Close()
+	v, err := redis.String(c.Do("GET", k))
+	if err != nil {
+		log.Error("Get Error: ", err)
+		return ""
+	}
+	return v
+}
+
+func GetZrangeList(k, min, max string) {
+
+}
+
+func GetSliceValue(k string) []string {
+	c := models.DB.Redis.Get()
+	defer c.Close()
+	v, err := redis.Strings(c.Do("GET", k))
+	if err != nil {
+		log.Error("Get Error: ", err)
+		return v
+	}
+	return v
+}
+
+func GetAllKeys(prexMatch string) []string {
+	c := models.DB.Redis.Get()
+	defer c.Close()
+	v, err := redis.Strings(c.Do("keys", prexMatch+"*"))
+	if err != nil {
+		log.Error("Get Error: ", err)
+		return v
+	}
+	return v
+}
+
+func SetKeyExpire(k string, ex int) {
+	c := models.DB.Redis.Get()
+	defer c.Close()
+	_, err := c.Do("EXPIRE", k, ex)
+	if err != nil {
+		log.Error("set error", err)
+	}
+}
+
+func CheckKey(k string) bool {
+	c := models.DB.Redis.Get()
+	defer c.Close()
+	exist, err := redis.Bool(c.Do("EXISTS", k))
+	if err != nil {
+		return false
+	} else {
+		return exist
+	}
+}
+
+func DelKey(k string) error {
+	c := models.DB.Redis.Get()
+	defer c.Close()
+	_, err := c.Do("DEL", k)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func SetJson(k string, data interface{}) error {
+	c := models.DB.Redis.Get()
+	defer c.Close()
+	value, _ := json.Marshal(data)
+	n, _ := c.Do("SETNX", k, value)
+	if n != int64(1) {
+		return errors.New("set failed")
+	}
+	return nil
+}
+
+func getJsonByte(key string) ([]byte, error) {
+	c := models.DB.Redis.Get()
+	jsonGet, err := redis.Bytes(c.Do("GET", key))
+	if err != nil {
+		return nil, err
+	}
+	return jsonGet, nil
+}
