6fe3a7840267dcd8a49bd0d9304354b9595460d9
haoxiongxiao <269812428@qq.com>
2019-04-03 15:37:16 +0800

    修改db文件中的函数

--- models/auto_migrate.go
+++ models/auto_migrate.go
@@ -6,12 +6,8 @@
 package models
 
 import (
-	"database/sql"
-	"fmt"
-
 	"github.com/jinzhu/gorm"
 	"github.com/lexkong/log"
-	"github.com/spf13/viper"
 )
 
 func autoMigrate(db *gorm.DB) {
@@ -29,40 +25,4 @@ func autoMigrate(db *gorm.DB) {
 		err != nil {
 		log.Error("自动建表失败", err)
 	}
-}
-
-func Syncdb() {
-	autoMigrate(DB.Mysql)
-	CreateDB()
-	InitMysql()
-
-}
-
-func CreateDB() {
-	username := viper.GetString("mysql.username")
-	password := viper.GetString("mysql.password")
-	addr := viper.GetString("mysql.addr")
-	name := viper.GetString("mysql.name")
-
-	config := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=%t&loc=%s",
-		username,
-		password,
-		addr,
-		name,
-		true,
-		"Asia/Shanghai")
-	//"Local")
-	sqlstring := fmt.Sprintf("CREATE DATABASE  if not exists `%s` CHARSET utf8mb4 COLLATE utf8mb4_general_ci", name)
-	db, err := sql.Open("mysql", config)
-	if err != nil {
-		panic(err.Error())
-	}
-	_, err = db.Exec(sqlstring)
-
-	if err != nil {
-		log.Errorf(err, "创建表%s失败", name)
-	} else {
-		fmt.Printf("Created Database %s  created", name)
-	}
-	defer db.Close()
 }
